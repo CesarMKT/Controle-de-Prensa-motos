951477e37d779692bd2eb1d697dcfe49dfb2587b
SamuelHD <47918944+SamuelHD@users.noreply.github.com>
2021-07-31 13:17:20 -0300

    ajuste contador troca de função
    Feito na fabrica ajustado e testado final com melhor tempo e ajuste do contador

---
+++ prensa_motos__em_placa_BoxInPaper_Ver3_rev10/EEPROM.ino
@@ -0,0 +1,71 @@
+//EEPROM  - -
+//procedimentos para persistir variaveis na memoria do sistema apos cada configuração escolhida em tempo de usuario
+
+void EEPROMIniciar()
+{
+
+  //EEPROM - -
+  // realizar leitura da EEPROM para alternar enderço de escrita constante evitando fadiga prematura dos registros da EEPROM
+  marcador = EEPROMReadLong(0); //Ler marcador que tem posição fixa para atualizar posição do contador variavel
+  if (marcador > 990)
+    marcador = 4; // verifica se a nova posição e maior que o final da memoria - se for zera marcador
+  if (marcador < 4)
+    marcador = 4;                      // na primeira utilização do programa o marcador deve ser definido no segundo emdereço para iniciar a contagem
+  contador = EEPROMReadLong(marcador); // memoriza contador na variavel
+  if (contador != EEPROMReadLong(marcador - 4))
+    marcador = marcador + 4; // se o contador == ao anterior evita reescrever o marcador senão
+  //cria nova posição para gravar contagem do ciclo
+  if (marcador > 990)
+    marcador = 4; // verifica se a nova posição e maior que o final da memoria - se for zera marcador
+  if (contador != EEPROMReadLong(marcador))
+    EEPROMWriteLong(marcador, contador); // verifica se o valor é diferente no endereço, se for atualiza
+  if (marcador != EEPROMReadLong(0))
+    EEPROMWriteLong(0, marcador); // por ultimo grava novo endereço na memoria do novo ciclo caso for diferente
+  pot1 = EEPROMReadInt(996);
+
+} // fim do tratamento para evitar fadiga da EEPROM
+
+void EEPROMWriteLong(int address, unsigned long value)
+{
+  byte four = (value & 0xFF);
+  byte three = ((value >> 8) & 0xFF);
+  byte two = ((value >> 16) & 0xFF);
+  byte one = ((value >> 24) & 0xFF);
+  EEPROM.write(address, four);
+  EEPROM.write(address + 1, three);
+  EEPROM.write(address + 2, two);
+  EEPROM.write(address + 3, one);
+} //end EEPROMWriteLong
+
+//OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+
+unsigned long EEPROMReadLong(int address)
+{
+  long four = EEPROM.read(address);
+  long three = EEPROM.read(address + 1);
+  long two = EEPROM.read(address + 2);
+  long one = EEPROM.read(address + 3);
+  return ((four << 0) & 0xFF) + ((three << 8) & 0xFFFF) + ((two << 16) & 0xFFFFFF) + ((one << 24) & 0xFFFFFFFF);
+} //end EEPROMReadLong
+
+//EEPROM  - -
+//procedimentos para persistir variaveis na memoria do sistema apos cada configuração escolhida em tempo de usuario
+
+void EEPROMWriteInt(int address, int value)
+{
+  byte two = (value & 0xFF);
+  byte one = ((value >> 8) & 0xFF);
+  EEPROM.write(address, two);
+  EEPROM.write(address + 1, one);
+
+} //end EEPROMWriteint
+
+//OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+
+unsigned int EEPROMReadInt(int address)
+{
+  int two = EEPROM.read(address);
+  int one = EEPROM.read(address + 1);
+
+  return ((two << 0) & 0xFF) + ((one << 8) & 0xFFFF);
+} //end EEPROMReadint
---
+++ prensa_motos__em_placa_BoxInPaper_Ver3_rev10/semiAuto.ino
@@ -0,0 +1,67 @@
+void semiAuto()
+{
+
+  timerAlimentador = millis();
+  btPedal = LOW;
+  btAlimentador = LOW;
+  ligaPrensa = LOW;
+  ligaLuz = LOW;
+  ligaAlimentador = LOW;
+  intSensor = LOW;
+  offSensor = LOW;
+  sensor = LOW;
+  int potSemiAuto = pot1;
+  if (pot1 <= 350)
+    potSemiAuto = 350;
+  delay(10);
+  atualizar();
+  delay(10);
+
+  //chave seletora 2/4 manual com alimentador A0 = 14 LOW A1=15 HIGH semiAuto
+  while (!TstBit(PINC, 0) && TstBit(PINC, 1))
+  {
+
+    //      chave seletora de posição no modo 3/4 manual para controle de batida uma por vez
+
+    //        -Acionamento por pedal ou bimanual  se o timer tiver passado o valor de timer
+    if (bimanual() && millis() - timerAlimentador >= potSemiAuto * 2)
+    {
+      ligaAlimentador = HIGH; // liga alimentador
+      ligaPrensa = HIGH;      // só liga prensa  se valor superior ida e volta alimentador
+    }
+    if (offSensor)
+    { // desliga se a prensa retornar antes do alimentador *2
+      offSensor = LOW;
+      ligaPrensa = LOW;
+    } // end prensa off por tempo
+    if (intSensor && ligaPrensa)
+    {
+
+      intSensor = LOW;
+      sensor = HIGH;
+      ligaAlimentador = HIGH;
+      btAlimentador = HIGH;
+      timerAlimentador = millis();
+
+    } //end if liga prensa/alimentador
+
+    //Alimentador ligar quando prensa chegar no sensor e ficar ligado pelo periodo de tempo do potenciometro
+    if (!btAlimentador && !TstBit(PIND, BtAlimentador) && millis() - timerAlimentador >= potSemiAuto * 2)
+    {
+      ligaAlimentador = HIGH;
+      btAlimentador = HIGH;
+      timerAlimentador = millis();
+    }
+    if (btAlimentador && TstBit(PIND, BtAlimentador))
+      btAlimentador = LOW;
+    if (millis() - timerAlimentador >= potSemiAuto && sensor)
+    {
+      sensor = LOW;
+      ligaAlimentador = LOW;
+      contar(); //conta as peças produzidas
+    }
+    atualizar(); //atualiza reles
+
+  } //end while
+
+} //end semiAuto
